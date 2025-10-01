3f224274aa3efaae133d4270b523ba6af932c8a0
Mukadus <mukadus.js@tafsol.com>
2025-10-01 15:38:35 +0500

    Enhance Landlord Insight Templates and UI Components
    - Added new spacing variable in typography.css for consistent layout.
    - Integrated API data fetching in LandlordInsightTemplate for dynamic content.
    - Updated LandlordInsightDetailTemplate to include a BreadCrumbSection for improved navigation.
    - Refactored PopOver component to use value instead of label for click events.
    - Made CSS adjustments across various components for better responsiveness and visual consistency.

--- src/components/Template/LandlordInsightTemplate/LandlordInsightDetailTemplate/index.jsx
+++ src/components/Template/LandlordInsightTemplate/LandlordInsightDetailTemplate/index.jsx
@@ -1,13 +1,24 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+"use client";
+import BreadCrumbSection from "@/components/organisms/BreadCrumbSection";
+import { landlordInsightDetailData } from "@/developmentContext/landlordInsight";
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import classes from "./LandlordInsightDetailTemplate.module.css";
 
 export default function LandlordInsightDetailTemplate({ slug = "" }) {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(landlordInsightDetailData ?? null);
 
   return (
     <Container fluid>
       <Row>
-        <Col lg={12}></Col>
+        <Col lg={12} className={classes.breadCrumbSection}>
+          <BreadCrumbSection
+            title="Landlord Insights"
+            title2="Landlord Details"
+            showStatusButton={true}
+            handleStatusButton={() => {}}
+          />
+        </Col>
       </Row>
     </Container>
   );
--- src/components/organisms/BreadCrumbSection/index.jsx
+++ src/components/organisms/BreadCrumbSection/index.jsx
@@ -1,5 +1,39 @@
 import React from "react";
+import classes from "./BreadCrumbSection.module.css";
+import Button from "@/components/atoms/Button";
+import clsx from "clsx";
 
-export default function BreadCrumbSection() {
-  return <div></div>;
+export default function BreadCrumbSection({
+  title,
+  title2,
+  showStatusButton,
+  label,
+  handleStatusButton,
+  leftIcon,
+}) {
+  return (
+    <div className={classes.breadCrumbSection}>
+      <div className={classes.headingTitle}>
+        {title && (
+          <h6 className={clsx(classes.title, "fs18 fw600")}>{title}</h6>
+        )}
+        {title && title2 && (
+          <span className={clsx(classes.separator, "fs24")}>{">"}</span>
+        )}
+        {title2 && (
+          <h6 className={clsx(classes.title2, "fs18 fw600")}>{title2}</h6>
+        )}
+      </div>
+      {showStatusButton && (
+        <div className={classes.statusButton}>
+          <Button
+            onClick={handleStatusButton}
+            label={label}
+            variant={"transparent"}
+            leftIcon={leftIcon}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
